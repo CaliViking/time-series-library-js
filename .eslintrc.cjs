b8cdd4efb2429ced0686fb97f331dbf5e7216950
Niels Andersen <niels.andersen@outlook.com>
2022-05-31 16:33:57 -0700

    Minor eslint clean up. It still does not lint typescript. Not sure why.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',

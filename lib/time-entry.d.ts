96cf46cd12187bc5601e3356c6d0840b923ef2fd
Niels Andersen <niels.andersen@outlook.com>
2022-07-08 09:30:11 -0700

    Added new types for TimeEntryArray

--- lib/time-entry.d.ts
+++ lib/time-entry.d.ts
@@ -1,6 +1,15 @@
 import { Severity } from './severity.js';
+/**  t = timestamp, v = value, s = severity */
 export declare type TimeEntry = {
     t: number;
     v: unknown;
     s?: Severity;
 };
+/** Extends TimeEntry, id = sourceId  */
+export declare type NamedTimeEntry = {
+    id: string;
+} & TimeEntry;
+/** Array containing [timestamp, value, status code] */
+export declare type TimeEntryArray = [number, unknown, number];
+/** Array containing [sourceId, timestamp, value, status code] */
+export declare type NamedTimeEntryArray = [string, ...TimeEntryArray];
--- src/time-entry.ts
+++ src/time-entry.ts
@@ -1,4 +1,10 @@
 import { Severity } from './severity.js';
 
-// t = timestamp, v = value, s status code
+/**  t = timestamp, v = value, s = severity */
 export type TimeEntry = { t: number; v: unknown; s?: Severity };
+/** Extends TimeEntry, id = sourceId  */
+export type NamedTimeEntry = { id: string } & TimeEntry;
+/** Array containing [timestamp, value, status code] */
+export type TimeEntryArray = [number, unknown, number];
+/** Array containing [sourceId, timestamp, value, status code] */
+export type NamedTimeEntryArray = [string, ...TimeEntryArray];

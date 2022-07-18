a9a71c89ebd82a8db75ca1dac7f46a8de508e7b1
Niels Andersen <niels.andersen@outlook.com>
2022-07-17 22:57:10 -0700

    Added vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // globals: true,
+    watch: false,
+    watchExclude: ['**/node_modules/**', '**/dist/**', '**/data/**', '**/digest/**', '**/build/**', '**/collectors/**'],
+    include: ['test/**/*'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
+  },
+});

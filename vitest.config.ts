bf71318cd6449507c91650cc3286039f7b32c65e
Niels Andersen <niels.andersen@outlook.com>
2022-07-18 22:46:10 -0700

    Fixed Vitest issues No functional changes Improved error messages for invalid type

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     // globals: true,
-    watch: false,
-    watchExclude: ['**/node_modules/**', '**/dist/**', '**/data/**', '**/digest/**', '**/build/**', '**/collectors/**'],
+    // watch: false,
+    watchExclude: ['node_modules/**', 'dist/**', 'data/**', 'digest/**', 'build/**', 'collectors/**'],
     include: ['test/**/*'],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
+    exclude: ['**/node_modules/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
   },
 });

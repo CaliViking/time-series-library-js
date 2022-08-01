877950f8a9f2474a51dadda20615d9d2f82544f4
Niels Andersen <niels.andersen@outlook.com>
2022-07-31 23:27:03 -0700

    Return a variable of the requested type

--- dist/values.d.ts
+++ dist/values.d.ts
@@ -13,3 +13,9 @@ export declare const NumberDataType: number;
 export declare const BooleanDataType: boolean;
 export declare const StringDataType: string;
 export declare const ObjectDataType: object;
+/**
+ * A helper function that returns a JavaScript data type of the type that is described in the string. See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ * @param typeName The string that represents tha type name. Array Types: 'Float64Array', 'Uint8Array', 'string[]', 'object[]'. Value Types: 'number', 'boolean', 'string', 'object'
+ * @returns A variable of that type. The value in the variable is irrelevant, it's type is what we are looking for
+ */
+export declare function getDataTypeFromTypeName(typeName: string): string | number | boolean | object;
--- dist/values.js
+++ dist/values.js
@@ -9,3 +9,26 @@ export const BooleanDataType = false;
 // eslint-disable-next-line @typescript-eslint/no-inferrable-types
 export const StringDataType = '';
 export const ObjectDataType = {};
+/**
+ * A helper function that returns a JavaScript data type of the type that is described in the string. See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ * @param typeName The string that represents tha type name. Array Types: 'Float64Array', 'Uint8Array', 'string[]', 'object[]'. Value Types: 'number', 'boolean', 'string', 'object'
+ * @returns A variable of that type. The value in the variable is irrelevant, it's type is what we are looking for
+ */
+export function getDataTypeFromTypeName(typeName) {
+    switch (typeName) {
+        case 'Float64Array':
+        case 'number':
+            return NumberDataType;
+        case 'Uint8Array':
+        case 'boolean':
+            return BooleanDataType;
+        case 'string[]':
+        case 'string':
+            return StringDataType;
+        case 'object[]':
+        case 'object':
+            return ObjectDataType;
+        default:
+            throw Error(`Unknown value array type: ${typeName}`);
+    }
+}
